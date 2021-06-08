bd58d5b3cc0a479cc264a373b88cc9ce80dff79f
Eric <22939634+ericDevSantana@users.noreply.github.com>
2021-06-07 23:40:36 -0700

    keyboard layout

--- src/components/Display.js
+++ src/components/Display.js
@@ -4,7 +4,7 @@ import styles from "./Display.module.css";
 const Display = (props) => {
   return (
     <div className={styles.display} id="display">
-      <div>00000000</div>
+      <h1>00000000</h1>
     </div>
   );
 };
